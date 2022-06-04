b8a65775aec8dd3161a2d0c19fc447c783a93016
z3oxs <z3ox1s@protonmail.com>
2022-06-04 12:10:45 -0300

    ✨ | Checking if "xclip" exists

--- utils/saveToClipboard.go
+++ utils/saveToClipboard.go
@@ -1,20 +1,46 @@
 package utils
 
+/*
+#cgo CFLAGS: -Wall
+#cgo LDFLAGS: 
+
+#include "../cgo/clipboard.c"
+*/
+import "C"
+
 import (
+    "log"
     "image"
     "os/exec"
-    "log"
+    "runtime"
+    "strings"
 
     "github.com/go-vgo/robotgo"
 )
 
 func SaveToClipboard(screenshot image.Image) {
-    robotgo.Save(screenshot, "/tmp/screenshot.png")
+    if runtime.GOOS == "linux" {
+        whereIsXClip, err := exec.Command("which", "xclip").Output()
+        if err != nil {
+            log.Fatal(err)
 
-    cmd := exec.Command("xclip", "-selection", "clipboard", "-t", "image/png", "-i", "/tmp/screenshot.png")
+        }
+        
+        if strings.Contains(string(whereIsXClip), "not found") {
+            log.Fatal("XClip was not found, are you sure that you have installed?")
 
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
+        }
+
+        robotgo.Save(screenshot, "/tmp/screenshot.png")
+
+        cmd := exec.Command("xclip", "-sel", "clipboard", "-t", "image/png", "-i", "/tmp/screenshot.png")
+
+        if err := cmd.Run(); err != nil {
+            log.Fatal(err)
+
+        }
+
+    } else if runtime.GOOS == "windows" {
 
     }
 }
