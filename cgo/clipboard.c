1694691315ee90f4b5d8b576f6ffeec48f08ff05
z3oxs <z3ox1s@protonmail.com>
2022-06-07 22:11:13 -0300

    ⚙️ | Some fixes

--- cgo/clipboard.c
+++ cgo/clipboard.c
@@ -3,17 +3,16 @@
 #if defined (WINDOWS)
     #include <windows.h>
 
-    void setClipboard(char screenshot) {
-        const size_t screenshotSize = strlen(* screenshot) + 1;
-        const UINT cf = RegisterClipboardFormat("PNG");
+    void setClipboard(char * screenshot) {
+        const size_t screenshotSize = strlen(screenshot) + 1;
 
         HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, screenshotSize);
-        memcpy(GlobalLock(hMem), * screenshot, screenshotSize);
+        memcpy(GlobalLock(hMem), screenshot, screenshotSize);
         GlobalUnlock(hMem);
 
         OpenClipboard(NULL);
         EmptyClipboard();
-        SetClipboardData(cf, hMem);
+        SetClipboardData(CF_DIB, hMem);
 
         CloseClipboard();
     }
