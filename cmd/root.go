7e7035c5b420b9c5e60b055647a843b2656db25b
z3oxs <z3ox1s@protonmail.com>
2022-06-07 22:37:25 -0300

    ✨ | Version v1.0.7

--- cmd/root.go
+++ cmd/root.go
@@ -23,7 +23,6 @@ var rootCmd = cobra.Command {
         upload, _ := c.Flags().GetBool("upload")
         selectiongui, _ := c.Flags().GetBool("selectiongui")
         version, _ := c.Flags().GetBool("version")
-        format, _ := c.Flags().GetString("format")
 
         if version {
             fmt.Println("v1.0.7 - Stable")
@@ -37,16 +36,16 @@ var rootCmd = cobra.Command {
         }
 
         if fullscreen {
-           modes.Fullscreen(save, format, clipboard, output, upload)
+           modes.Fullscreen(save, clipboard, output, upload)
 
         } else if selection {
-            modes.Selection(save, format, clipboard, output, upload)
+            modes.Selection(save, clipboard, output, upload)
 
         } else if selectiongui {
-            modes.SelectionGUI(save, format, clipboard, output, upload)
+            modes.SelectionGUI(save, clipboard, output, upload)
 
         } else if display != -1 {
-            modes.Display(save, format, clipboard, output, upload, display)
+            modes.Display(save, clipboard, output, upload, display)
 
         } else {
             c.Help()
@@ -73,7 +72,6 @@ func init() {
     rootCmd.PersistentFlags().BoolP("upload", "u", false, "Upload the screenshot to AnonFiles")
     rootCmd.PersistentFlags().BoolP("selectiongui", "g", false, "Selection with GUI mode.")
     rootCmd.PersistentFlags().BoolP("version", "v", false, "Show version")
-    rootCmd.PersistentFlags().StringP("format", "F", "png", "Output format (png, jpg)")
 }
 
 func Execute() {
